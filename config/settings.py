51ea487639d75a5c2b27d779666d9b38585c496b
unknown <jrexxtreme@gmail.com>
2026-05-28 11:45:08 -0300

    backend

--- config/settings.py
+++ config/settings.py
@@ -64,10 +64,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
