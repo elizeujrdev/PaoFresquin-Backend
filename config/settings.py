283873c784a8964bee9d10eb33f85cba7e6f89c2
unknown <jrexxtreme@gmail.com>
2026-05-28 11:07:08 -0300

    backend

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dev-paofresquim-change-in-production"
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
